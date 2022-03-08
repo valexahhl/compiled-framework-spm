1b1499cd0e7cd2321c1ab5cf27dac2477cc9bfe2
Vlad Alexa <vlad.alexa@hedgehoglab.com>
2022-03-08 16:08:00 +0000

    renamed library

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "compiled-framework-spm",
-            targets: ["compiled-framework-spm"]
+            name: "HERE SDK",
+            targets: ["heresdk"]
         )
     ],
     dependencies: [
