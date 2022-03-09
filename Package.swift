186c17503f50252c4c06c3708a8f39450026bebc
Vlad Alexa <vlad.alexa@hedgehoglab.com>
2022-03-09 09:52:26 +0000

    remove redirect

--- Package.swift
+++ Package.swift
@@ -21,8 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.binaryTarget(
 		   name: "heresdk",
-		   url: "https://github.com/valexahhl/compiled-framework-spm/releases/download/0.0.1/heresdk-navigate-ios-4.10.4.0.7956.zip",
+		   url: "https://objects.githubusercontent.com/github-production-release-asset-2e65be/467557129/7f175314-5198-4b53-89bc-a590680c6c7b?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220309%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220309T095044Z&X-Amz-Expires=300&X-Amz-Signature=042e903937b5208e8cbd23b7c623e9965d67232da7101491b35980bd4a115747&X-Amz-SignedHeaders=host&actor_id=96238834&key_id=0&repo_id=467557129&response-content-disposition=attachment%3B%20filename%3Dheresdk-navigate-ios-4.10.4.0.7956.zip&response-content-type=application%2Foctet-stream",
 		   checksum: "059b81db97562c6c2f12852e439fbca13f9bd174c4a9712ab3e48f6cad7be142"
 		)
     ]
 )
+
+
